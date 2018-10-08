757eece6e458bcd6fde1f68ad2383b5db6f53ecb
Adam <adoantal@gmail.com>
2018-10-08 16:16:11 +0200

    database V1 attempt 2

--- controllers/import_from_here/web_sec.sql
+++ controllers/import_from_here/web_sec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2018 at 03:50 PM
+-- Generation Time: Oct 08, 2018 at 04:15 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
