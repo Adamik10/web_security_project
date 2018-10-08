c8140273bc965c4661ebba52d0a69c1cf36bb38a
Adam <adoantal@gmail.com>
2018-10-08 15:54:12 +0200

    database V1 set up

---
+++ controllers/database.php
@@ -0,0 +1,18 @@
+<?php
+
+try{
+    $sUserName = 'root';
+    $sPassword = '';
+    $sConnection = "mysql:host=localhost; dbname=web_sec; charset=utf8";
+
+    $aOptions = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    );
+
+    $db = new PDO( $sConnection, $sUserName, $sPassword, $aOptions );
+
+}catch( PDOException $e){
+    echo 'error';
+    exit();
+}
