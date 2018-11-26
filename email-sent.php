87a10d2ee26501f75df516fb116860f3a16d2dea
Adam <adoantal@gmail.com>
2018-11-26 12:35:04 +0100

    more fixes for handin

--- email-sent.php
+++ email-sent.php
@@ -10,7 +10,7 @@ require_once('components/top.php');
     <!-- TEMPLATE START -->
 <div class="card align-self-center card-custom mt-2 mb-2">
 
-<p style="text-align:center; margin-top:15px;">Verification email has been sent. Please verify your account.</p>
+<p style="text-align:center; margin-top:15px;">Verification email has been sent. Please verify your account. <br><br> Please note that it might take up to 5 minutes for the verifiation email to arrive.</p>
 
 </div>
     <!-- TEMPLATE END -->
